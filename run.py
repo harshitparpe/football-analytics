7dc69d80cc025855364807e3b28079448941ed55
hprs <152280821+harshitparpe@users.noreply.github.com>
2026-08-09 11:33:19 +0530

    Modify run.py to use dynamic port from environment
    Updated the run configuration to use a PORT environment variable.

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
+import os
+
 from api import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    import os
-    debug = os.environ.get('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=5000, debug=debug)
+if __name__ == "__main__":
+    debug = os.environ.get("FLASK_ENV") == "development"
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=debug
+    )
